c620ebd401f8c9107e1b1f703b89c25c40f8f245
Julian Schmuckli <julian@schmuckli.net>
2019-04-25 23:59:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>schmuckli.dev</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <h1>Welcome!</h1>
+  </v-container>
+</template>
