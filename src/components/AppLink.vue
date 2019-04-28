983a245c46cfc1be415b24f28cca79bd70e3e1fc
Julian Schmuckli <julian@schmuckli.net>
2019-04-29 00:31:14 +0200

    Added app links

---
+++ src/components/AppLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <a :href="url" target="_blank" class="outer_card">
+    <v-card v-ripple :style="card_style">
+      <div :style="image_style">
+      </div>
+      <span class="app_name">{{ name }}</span>
+    </v-card>
+  </a>
+</template>
+<script>
+export default {
+  name: "AppLink",
+  props: {
+    name: String,
+    url: String,
+    app_icon: String,
+    color: String
+  },
+  computed: {
+    card_style(){
+      return "background:" + this.color + ";width:100%;height:200px;border-radius:12px;text-align:center;";
+    },
+    image_style(){
+      return "background-image:url(" + this.app_icon + ");background-size:150px 150px;height:150px;width:150px;margin-left:auto;margin-right:auto;";
+    }
+  }
+}
+</script>
+<style>
+.outer_card{
+  text-decoration: none;
+}
+.app_name{
+  font-size: 20px;
+  text-decoration: none;
+  color:white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,47 @@
 <template>
   <v-container>
     <h1>Welcome!</h1>
+    <div class="app_container">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="app in apps" :key="app.key" style="padding:10px;">
+          <AppLink :name="app.name" :url="app.url" :app_icon="app.app_icon" :color="app.color" />
+        </v-flex>
+      </v-layout>
+    </div>
   </v-container>
 </template>
+<script>
+import AppLink from "../components/AppLink";
+
+export default {
+  name: "Home",
+  data(){
+    return {
+      apps: [
+        {
+          key: "wallet",
+          name: "Wallet",
+          url: "https://wallet.schmuckli.dev/",
+          app_icon: "https://wallet.schmuckli.dev/android-icon-192x192.png",
+          color: "#9D6447"
+        },
+        {
+          key: "shoppinglist",
+          name: "Shoppinglist",
+          url: "https://shoppinglist.schmuckli.dev/",
+          app_icon: "https://shoppinglist.schmuckli.dev/android-icon-192x192.png",
+          color: "#24919B"
+        }
+      ]
+    }
+  },
+  components: {
+    AppLink
+  }
+}
+</script>
+<style scoped>
+.app_container{
+  margin-top: 30px;
+}
+</style>
