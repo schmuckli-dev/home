3b6560fee4b639c7c267f7678ca39003d20129ae
Julian Schmuckli <julian@schmuckli.net>
2019-05-15 18:20:11 +0200

    Improved footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -2,16 +2,24 @@
   <footer>
     <v-container>
       <v-layout row wrap>
-        <v-flex sm6>
+        <v-flex xs12 sm6 md4>
           <a @click="openPrivacyPolicy">{{ $t("privacy_poliy") }}</a>
           <br />
           <a @click="openTermsOfService">{{ $t("terms_of_service") }}</a>
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          <RoundLink name="Github" icon="fab fa-github" link="https://www.github.com/schmuckli-dev" />
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          &copy; {{ year }} by schmuckli.dev
         </v-flex>
       </v-layout>
     </v-container>
   </footer>
 </template>
 <script>
+import RoundLink from "./RoundLink";
+
 export default {
   name: "Footer",
   methods: {
@@ -21,6 +29,14 @@ export default {
     openTermsOfService(){
       this.$router.replace("terms");
     }
+  },
+  computed: {
+    year(){
+      return (new Date()).getFullYear();
+    }
+  },
+  components: {
+    RoundLink
   }
 }
 </script>
---
+++ src/components/footer/RoundLink.vue
@@ -0,0 +1,40 @@
+<template>
+  <a :href="link" target="_blank">
+    <div class="round_link">
+      <v-icon v-html="icon"></v-icon>
+    </div>
+  </a>
+</template>
+<script>
+export default {
+  name: "RoundLink",
+  props: {
+    name: {
+      type: String
+    },
+    icon: {
+      type: String,
+      default: "fal fa-globe"
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style>
+.round_link{
+  border-radius: 20px;
+  border: 1px solid white;
+  width: 40px;
+  height: 40px;
+  font-size: 25px;
+  color: white;
+  transition: all 0.3s;
+}
+.round_link:hover{
+  background-color: white;
+  color: #232323;
+}
+</style>
