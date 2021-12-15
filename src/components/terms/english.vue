218b1aa109b23ee3d7328ef786e510722bffd579
Julian Schmuckli <julian@schmuckli.net>
2021-12-15 11:38:15 +0100

    Updated some terms

--- src/components/terms/english.vue
+++ src/components/terms/english.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <p>With the use of the schmuckli.dev services, the following terms of use shall apply:</p>
-    <p>&nbsp;</p>
-    <h3>Firebase</h3>
-    <p>For all services Firebase is used as primary backend. Therefore all terms of use of firebase.com and google.com apply automatically.</p>
-    <h3>Google Assistant</h3>
-    <p>The Google Assistant is used on a voluntary basis by the user. During conversations, app data such as the name of lists and their content are sent to Google.
-    The accounts are automatically linked with the link to Google under "Settings". The Google Assistant will then connect to your account via email address.</p>
+    <p>Terms only available in German at the moment. The terms in German apply.</p>
   </div>
 </template>
 <script>
--- src/components/terms/german.vue
+++ src/components/terms/german.vue
@@ -1,16 +1,45 @@
 <template>
   <div>
-    <p>Mit der Nutzung der schmuckli.dev Services gelten folgende Nutzungsbedingungen in Kraft:</p>
-    <p>&nbsp;</p>
+    <p>
+      Mit der Nutzung der schmuckli.dev Applikationen gelten folgende
+      Bestimmungen:
+    </p>
+    <h3>Servicevertrag Dienste</h3>
+    <p>
+      schmuckli.dev behält sich das Recht vor, Dienste und Services wie die
+      Speicherung der Daten in der Cloud oder Synchronisationsdienste 
+      zu verändern, einzustellen und zu löschen.
+    </p>
+    <h3>Servicevertrag Applikationen</h3>
+    <p>
+      schmuckli.dev behält sich das Recht vor, den Support für alle Applikationen
+      (basierend auf Web, Android und iOS) einzustellen.
+    </p>
+    <h3>Käufe und Transaktionen</h3>
+    <p>
+      Beim Kauf von Applikationen oder Dienstleistungen über den Apple App Store
+      sowie über den Google Play Store gelten die Bestimmungen der jeweiligen
+      Händler.
+    </p>
     <h3>Firebase</h3>
-    <p>Für alle Services wird als primäres Backend Firebase verwendet. Daher gelten auch sämtliche Nutzungsbedingungen von firebase.com und google.com.</p>
+    <p>
+      Bei einer Kleinzahl an Apps wird als primäres Backend Firebase verwendet.
+      Daher gelten auch sämtliche Nutzungsbedingungen von firebase.com und
+      google.com.
+    </p>
     <h3>Google Assistant</h3>
-    <p>Die Nutzung des Google Assistants erfolgt auf freiwilliger Basis des Nutzers. Während den Konversationen werden Appdaten, wie zu Beispiel Name von Listen und deren Inhalt an Google gesendet.
-    Mit der Verlinkung zu Google unter den "Einstellungen" werden automatisch die Konten verknüpft. Der Google Assistant wird sich dann per E-Mail Adresse mit Ihrem Konto verbinden.</p>
+    <p>
+      Die Nutzung des Google Assistants erfolgt auf freiwilliger Basis des
+      Nutzers. Während den Konversationen werden Appdaten, wie zu Beispiel Name
+      von Listen und deren Inhalt an Google gesendet. Mit der Verlinkung zu
+      Google unter den "Einstellungen" werden automatisch die Konten verknüpft.
+      Der Google Assistant wird sich dann per E-Mail Adresse mit Ihrem Konto
+      verbinden.
+    </p>
   </div>
 </template>
 <script>
 export default {
-  name: "TermsGerman"
-}
+  name: "TermsGerman",
+};
 </script>
